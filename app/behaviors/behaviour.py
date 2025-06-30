46efffdd699774de19be8d93053f20edd04f6f79
Dmytro <DmitryUglach@gmail.com>
2025-06-30 22:40:41 +0300

    hardworking

--- app/behaviors/behaviour.py
+++ app/behaviors/behaviour.py
@@ -1,7 +1,7 @@
 from collections import deque
 from dataclasses import dataclass, field
 from typing import Callable, ClassVar
-from ..bus.message_broker import MessageTypes, Message
+from ..bus.message_broker.types import MessageTypes, Message
 from ..config import Behaviours
 from ..objects.actor.actor import Actor
 
@@ -18,20 +18,29 @@ BehaviourFn = Callable[[Actor, Message], BehaviourAction]
 # Base Behaviour class
 class Behaviour:
     name: ClassVar[Behaviours] = Behaviours.BEHAVIOUR
-    message_handlers: ClassVar[dict[MessageTypes, deque[BehaviourFn]]] = {}
+    message_handlers: ClassVar[dict[MessageTypes, tuple[BehaviourFn, ...]]] = {}
+    supported_receivers = (Actor,)
+
 
     @classmethod
-    def on_message(cls, actor: Actor, message: Message) -> deque[BehaviourAction]:
+    def on_message(cls, receiver: Actor, message: Message) -> deque[BehaviourAction]:
         response_actions: deque[BehaviourAction] = deque()
-        if cls.can_respond_to(message.message_type):
+        if cls.can_handle(receiver, message.message_type):
             for handler in cls.message_handlers[message.message_type]:
-                action = handler(actor, message)
+                action = handler(receiver, message)
                 if action:
                     response_actions.append(action)
 
         return response_actions
 
     @classmethod
-    def can_respond_to(cls, message_type: MessageTypes) -> bool:
-        return message_type in cls.message_handlers
+    def can_handle(cls, receiver: Actor, message_type: MessageTypes) -> bool:
+        return (
+                isinstance(receiver, cls.supported_receivers)
+                and message_type in cls.message_handlers
+        )
+
+    @classmethod
+    def register_handlers(cls):
+        pass
 
--- app/behaviors/coordinate_holder/moveable.py
+++ app/behaviors/coordinate_holder/moveable.py
@@ -1,35 +1,67 @@
 from collections import deque
-from typing import Optional, ClassVar
+from typing import Optional
 
-from ..behaviour import Behaviour, BehaviourAction, BehaviourFn
+from ..behaviour import Behaviour, BehaviourAction
 from app.config import Behaviours
 from ...bus.message_broker.types import MessageTypes, Message, PushedByPayload
-from ...objects.actor.actor import Actor
+from ...objects.actor.coordinate_holder import CoordinateHolder
+from ...objects.actor.static_object import StaticObject
+from ...objects.actor.unit import Unit
+
 
 class Moveable(Behaviour):
     name = Behaviours.MOVEABLE
-    message_handlers: ClassVar[dict[MessageTypes, deque[BehaviourFn]]] = {}
+    supported_receivers = (CoordinateHolder, StaticObject, Unit)
 
-    @staticmethod
-    def pushed_by(receiver: Actor, message: Message) -> Optional[BehaviourAction]:
-        if isinstance(message.payload, PushedByPayload):
-            pushed_payload = message.payload
-            
-            if pushed_payload.force > 10:
-                return BehaviourAction(
-                    behaviour=Behaviours.MOVEABLE, 
-                    method_name="knockback",
-                    kwargs={"direction": pushed_payload.direction, "distance": pushed_payload.force // 5}
+    @classmethod
+    def handle_pushed_by_as_unit(cls, unit: Unit, pushed_payload: PushedByPayload) -> BehaviourAction:
+        pass
+
+    @classmethod
+    def handle_pushed_by_as_coordinate_holder(cls, unit: CoordinateHolder, pushed_payload: PushedByPayload) -> BehaviourAction:
+        pass
+
+    @classmethod
+    def handle_pushed_by_as_static_object(cls, unit: StaticObject, pushed_payload: PushedByPayload) -> BehaviourAction:
+        pass
+
+    @classmethod
+    def pushed_by(cls, receiver: CoordinateHolder, message: Message) -> Optional[deque[BehaviourAction]]:
+        if not isinstance(message.payload, PushedByPayload):
+            raise TypeError(f"Expected PushedByPayload, got {type(message.payload)}")
+
+        actions_queue: deque[BehaviourAction] = deque()
+
+        if isinstance(receiver, Unit):
+            actions_queue.append(
+                BehaviourAction(
+                    behaviour=Behaviours.MOVEABLE,
+                    method_name="handle_pushed_by_as_unit",
+                    kwargs=message.payload.__dict__
                 )
-            else:
-                return BehaviourAction(
-                    behaviour=Behaviours.MOVEABLE, 
-                    method_name="step_back",
-                    kwargs={"direction": pushed_payload.direction}
+            )
+
+        if isinstance(receiver, StaticObject):
+            actions_queue.append(
+                BehaviourAction(
+                    behaviour=Behaviours.MOVEABLE,
+                    method_name="handle_pushed_by_as_static_object",
+                    kwargs=message.payload.__dict__
                 )
-        
-        return BehaviourAction(behaviour=Behaviours.MOVEABLE, method_name="step_back")
+            )
+
+        if isinstance(receiver, CoordinateHolder):
+            actions_queue.append(
+                BehaviourAction(
+                    behaviour=Behaviours.MOVEABLE,
+                    method_name="handle_pushed_by_as_coordinate_holder",
+                    kwargs=message.payload.__dict__
+                )
+            )
+
+        return actions_queue
 
     @classmethod
     def register_handlers(cls):
-        cls.message_handlers[MessageTypes.PUSHED_BY] = deque([cls.pushed_by])
+        existing = cls.message_handlers.get(MessageTypes.PUSHED_BY, ())
+        cls.message_handlers[MessageTypes.PUSHED_BY] = existing + (cls.pushed_by,)
