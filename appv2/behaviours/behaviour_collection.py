2afcb4f34b2c4a99edfabc29cc37b2b81e11db9c
Dmytro <DmitryUglach@gmail.com>
2025-07-08 21:55:52 +0300

    Refactoring v2

--- appv2/behaviours/behaviour_collection.py
+++ appv2/behaviours/behaviour_collection.py
@@ -1,14 +1,33 @@
-from typing import Self
+from typing import Self, Optional
 
 from appv2.engine.message_broker.types import MessageTypes
 from appv2.config import Behaviours
 from appv2.core.collection_base import CollectionBase
 from appv2.protocols.behaviour_protocol import BehaviourProtocol
+from appv2.registry.behaviour_registry import get_registry
 
 
-class BehavioursCollection(CollectionBase[Behaviours, BehaviourProtocol]):
-    def __init__(self, items: dict[Behaviours, BehaviourProtocol] | None = None):
+class BehaviourCollection(CollectionBase[Behaviours, Behaviours | BehaviourProtocol]):
+    def __init__(self, items: dict[Behaviours, Behaviours | BehaviourProtocol] | None = None):
         super().__init__(items or {})
 
-    def can_response_to(self, message_type: MessageTypes) -> Self:
-        return type(self)(self.filter(lambda a: a.can_handle(message_type)))
+    def __getitem__(self, item: Behaviours) -> Optional[BehaviourProtocol]:
+        value = self.data.get(item)
+
+        if isinstance(value, Behaviours):
+            behaviour = get_registry().get(value)
+            self.data[item] = behaviour
+            return behaviour
+
+        return value
+
+    def can_respond_to(self, message_type: MessageTypes) -> Self:
+        return type(self)(
+            self.filter(
+                lambda a: (
+                    get_registry().get(a).can_respond_to(message_type)
+                    if isinstance(a, Behaviours)
+                    else a.can_respond_to(message_type)
+                )
+            )
+        )
--- appv2/objects/actor.py
+++ appv2/objects/actor.py
@@ -1,20 +1,23 @@
 from collections import deque
-from typing import List, TYPE_CHECKING
+from typing import List
 
-from appv2.behaviours.behaviours_collection import BehavioursCollection
+from appv2.behaviours.behaviour_collection import BehaviourCollection
 from appv2.engine.message_broker.types import MessageBody
 from appv2.config import Behaviours
 from appv2.behaviours.types import BehaviourAction
+from appv2.protocols.actor_protocol import ActorProtocol
+from appv2.registry.behaviour_registry import get_registry
 
-class Actor:
+
+class Actor(ActorProtocol):
     def __init__(self, name: str = None):
         super().__init__(name)
         self.active = True
         self.pending_actions: deque[BehaviourAction] = deque()
-        self.__behaviours: BehavioursCollection = BehavioursCollection()
+        self.__behaviours: BehaviourCollection = BehaviourCollection()
 
     def on_message(self, message_body: MessageBody) -> deque[BehaviourAction]:
-        filtered_behaviours = self.__behaviours.can_response_to(message_body.message_type)
+        filtered_behaviours = self.__behaviours.can_respond_to(message_body.message_type)
         response_actions: deque[BehaviourAction] = deque()
         for behaviour in filtered_behaviours.values():
             response_actions.extend(behaviour.on_message(self, message_body))
@@ -27,9 +30,9 @@ class Actor:
     def is_behave_as_them(self, behaviours: List[Behaviours]) -> bool:
         return all(behaviour in self.__behaviours for behaviour in behaviours)
 
-    def add_behaviour(self, behaviour: Behaviours):
+    def add_behaviour(self, behaviour: Behaviours) -> None:
         behaviour_class = get_registry().get(behaviour)
-        return self.add_behaviour(behaviour_class)
+        self.__behaviours[behaviour] = behaviour_class
 
     def remove_behaviour(self, behaviour: Behaviours):
         self.__behaviours.pop(behaviour.name, None)
