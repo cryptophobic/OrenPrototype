d3122c475f64b3788625126a7f7ac55a45cf2d4c
Dmytro <DmitryUglach@gmail.com>
2025-07-15 18:04:23 +0300

    Migrating to Arcade

---
+++ app/core/animations.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Animations(Enum):
+    IDLE = "idle"
+    MOVE = "move"
+    ATTACK = "attack"
--- app/core/geometry/shape.py
+++ app/core/geometry/shape.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 
 class Shape:
-    def __init__(self, icon_path: Path):
+    def __init__(self, icon_path: Path, animations: dict[str, list[Path]] = None):
         self.icon_path: Path = icon_path
+        self.animations: dict[str, list[Path]] = animations or {}
         pass
