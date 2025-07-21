638477e8e5e0aac3609841e07f7a4fa846993de8
cryptophobic <dmitryuglach@gmail.com>
2025-07-22 00:58:16 +0300

    refactoring v2

--- app/boilerplate/test.py
+++ app/boilerplate/test.py
@@ -22,20 +22,20 @@ from app.core.vectors import CustomVec2i, CustomVec2f
 from app.protocols.collections.animation_collection_protocol import AnimationCollectionProtocol
 
 # Set how many rows and columns we will have
-ROW_COUNT = 30
-COLUMN_COUNT = 30
+ROW_COUNT = 20
+COLUMN_COUNT = 20
 
 # This sets the WIDTH and HEIGHT of each grid location
-WIDTH = 32
-HEIGHT = 32
+WIDTH = 64
+HEIGHT = 64
 
 # This sets the margin between each cell
 # and on the edges of the screen.
 MARGIN = 5
 
 CELL_WIDTH = 1.0  # logical cell size
-VELOCITY_PER_CELL_150MS = 1 / 0.5  # ~6.67 cells/sec
-VELOCITY_PER_CELL_300MS = 1 / 0.25
+VELOCITY_PER_CELL_150MS = 1 / 0.40  # ~6.67 cells/sec
+VELOCITY_PER_CELL_300MS = 1 / 0.40
 
 
 # Do the math to figure out our screen dimensions
@@ -44,10 +44,10 @@ WINDOW_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
 WINDOW_TITLE = "Array Backed Grid Buffered Example"
 
 class Player(arcade.Sprite):
-    def __init__(self, texture_list: list[arcade.Texture]):
+    def __init__(self, texture_list: list[arcade.Texture], position: CustomVec2i):
         super().__init__(texture_list[0])
         self.time_elapsed = 0
-        self.coordinates: CustomVec2i = CustomVec2i(12, 14)
+        self.coordinates: CustomVec2i = position
         self.velocity: CustomVec2f = CustomVec2f.zero()
         self.buffer: CustomVec2f = CustomVec2f(0.0, 0.0)
         self.cell_size = 1.0
@@ -62,50 +62,22 @@ class Player(arcade.Sprite):
             self.set_texture(self.cur_texture_index)
             self.cur_texture_index = (self.cur_texture_index + 1) % 5
 
-
     def update_movement(self, delta_time, keys_held):
         self.buffer += self.velocity * delta_time
-        moved = [self.buffer.x == 0, self.buffer.y == 0]
+        candidate = CustomVec2i(self.coordinates.x, self.coordinates.y)
 
         if abs(self.buffer.x) >= self.cell_size:
-            sign = 1 if self.buffer.x >= 0 else -1
-            step = sign * int(abs(self.buffer.x) // self.cell_size)
-            self.coordinates.x += step
-            self.buffer.x -= step * self.cell_size
-            moved[0] = True
+            step = int(self.buffer.x)
+            candidate.x += step
+            self.buffer.x -= step
 
         if abs(self.buffer.y) >= self.cell_size:
-            sign = 1 if self.buffer.y >= 0 else -1
-            step = sign * int(abs(self.buffer.y) // self.cell_size)
-            self.coordinates.y += step
-            self.buffer.y -= step * self.cell_size
-            moved[1] = True
+            step = int(self.buffer.y)
+            candidate.y += step
+            self.buffer.y -= step
 
-        if moved[0] and self.velocity.x:
-            # Refresh velocity if key still held
-            vx = 0.0
-            if arcade.key.RIGHT in keys_held:
-                vx += VELOCITY_PER_CELL_150MS
-            if arcade.key.LEFT in keys_held:
-                vx -= VELOCITY_PER_CELL_150MS
-            self.velocity.x = vx
+        return candidate
 
-            if vx == 0:
-                # Stop at cell if no input
-                self.buffer.x = 0.0
-
-        if moved[1] and self.velocity.y:
-            # Refresh velocity if key still held
-            vy = 0.0
-            if arcade.key.UP in keys_held:
-                vy += VELOCITY_PER_CELL_300MS
-            if arcade.key.DOWN in keys_held:
-                vy -= VELOCITY_PER_CELL_300MS
-            self.velocity.y = vy
-
-            if vy == 0:
-                # Stop at cell if no input
-                self.buffer.y = 0.0
 
 
 def slice_animation_strip(image_path: str, start_x: int, start_y: int, frame_width: int, frame_height: int, count: int) -> list[Texture]:
@@ -141,6 +113,7 @@ class GameView(arcade.View):
         self.grid_sprite_list = arcade.SpriteList()
         self.grid_sprites = []
         self.rocks = []
+        self.direction = 'front'
 
         # Create a list of solid-color sprites to represent each grid location
         for row in range(ROW_COUNT):
@@ -158,16 +131,23 @@ class GameView(arcade.View):
 
         self.animations: AnimationCollectionProtocol = AnimationCollection()
         self.animations.set(NpcAnimations.ARMED_RUN)
+        self.animations.set(NpcAnimations.ARMED_IDLE)
+        self.animations.set(NpcAnimations.ENEMY_RUN_ATTACK)
 
         # frames = slice_animation_strip("resources/craftpix/male/PNG/Sword_attack/Sword_attack_full.png",
         #                               0, 0, 64, 64, 8)
 
         self.keys_held = set()
-        self.player = Player(self.animations.get(NpcAnimations.ARMED_RUN).front)
-        self.player.scale = 1.0
+        self.player = Player(self.animations.get(NpcAnimations.ARMED_RUN).front, CustomVec2i(12, 14))
+        self.player.scale = 2.0
         self.player.position = self.get_tile_center(self.player.coordinates.x), self.get_tile_center(self.player.coordinates.y)
 
+        self.enemy = Player(self.animations.get(NpcAnimations.ENEMY_RUN_ATTACK).front, CustomVec2i(15, 10))
+        self.enemy.scale = 1.5
+        self.enemy.position = self.get_tile_center(self.enemy.coordinates.x), self.get_tile_center(self.enemy.coordinates.y)
+
         self.sprite_list.append(self.player)
+        self.sprite_list.append(self.enemy)
 
     def on_key_press(self, symbol, modifiers):
         if symbol == arcade.key.ESCAPE:
@@ -188,7 +168,30 @@ class GameView(arcade.View):
         self.keys_held.discard(symbol)
 
     def on_update(self, delta_time: float) -> bool | None:
-        self.player.update_movement(delta_time, self.keys_held)
+        candidate = self.player.update_movement(delta_time, self.keys_held)
+
+        if candidate.x != self.player.coordinates.x:
+            # Refresh velocity if key still held
+            vx = 0.0
+            if arcade.key.RIGHT in self.keys_held:
+                vx += VELOCITY_PER_CELL_150MS
+            if arcade.key.LEFT in self.keys_held:
+                vx -= VELOCITY_PER_CELL_150MS
+            self.player.velocity.x = vx
+            self.player.buffer.x = 0.0 if vx == 0 else self.player.buffer.x
+
+        if candidate.y != self.player.coordinates.y:
+            # Refresh velocity if key still held
+            vy = 0.0
+            if arcade.key.UP in self.keys_held:
+                vy += VELOCITY_PER_CELL_300MS
+            if arcade.key.DOWN in self.keys_held:
+                vy -= VELOCITY_PER_CELL_300MS
+            self.player.velocity.y = vy
+            self.player.buffer.y = 0.0 if vy == 0 else self.player.buffer.y
+
+        if self.enemy.coordinates != candidate:
+            self.player.coordinates = candidate
 
         start_x = self.get_tile_center(self.player.coordinates.x) + self.player.buffer.x * WIDTH
         start_y = self.get_tile_center(self.player.coordinates.y) + self.player.buffer.y * HEIGHT
@@ -196,12 +199,35 @@ class GameView(arcade.View):
         # Update sprite position based on grid coordinates
         self.player.center_x = start_x
         self.player.center_y = start_y
-        self.grid_sprites[self.player.coordinates.y][self.player.coordinates.x].color = arcade.color.RUST
+        # self.grid_sprites[self.player.coordinates.y][self.player.coordinates.x].color = arcade.color.RUST
         normalized = CustomVec2f(start_x, start_y) + CustomVec2f(self.player.velocity.x,
-                                                                 self.player.velocity.y).normalized().scale_to(WIDTH)
+                                                                 self.player.velocity.y).normalized().scale_to(WIDTH // 2)
 
         index = self.get_tile_index_from_pixel(normalized)
-        self.grid_sprites[index.y][index.x].color = arcade.color.GRANNY_SMITH_APPLE
+        # self.grid_sprites[index.y][index.x].color = arcade.color.GRANNY_SMITH_APPLE
+
+        if self.player.velocity.y < 0:
+            self.player.textures = self.animations.get(NpcAnimations.ARMED_RUN).front
+            self.direction = 'front'
+        elif self.player.velocity.y > 0:
+            self.player.textures = self.animations.get(NpcAnimations.ARMED_RUN).back
+            self.direction = 'back'
+        elif self.player.velocity.x < 0:
+            self.player.textures = self.animations.get(NpcAnimations.ARMED_RUN).left
+            self.direction = 'left'
+        elif self.player.velocity.x > 0:
+            self.player.textures = self.animations.get(NpcAnimations.ARMED_RUN).right
+            self.direction = 'right'
+        elif len(self.keys_held) == 0:
+            if self.direction == 'front':
+                self.player.textures = self.animations.get(NpcAnimations.ARMED_IDLE).front
+            if self.direction == 'back':
+                self.player.textures = self.animations.get(NpcAnimations.ARMED_IDLE).back
+            if self.direction == 'left':
+                self.player.textures = self.animations.get(NpcAnimations.ARMED_IDLE).left
+            if self.direction == 'right':
+                self.player.textures = self.animations.get(NpcAnimations.ARMED_IDLE).right
+
 
         self.sprite_list.update()
 
@@ -215,7 +241,7 @@ class GameView(arcade.View):
         start_y = self.get_tile_center(self.player.coordinates.y) + self.player.buffer.y * HEIGHT
 
         # Scale vector visually (e.g., exaggerate length for clarity)
-        normalized = CustomVec2f(start_x, start_y) + CustomVec2f(self.player.velocity.x, self.player.velocity.y).normalized().scale_to(WIDTH)
+        normalized = CustomVec2f(start_x, start_y) + CustomVec2f(self.player.velocity.x, self.player.velocity.y).normalized().scale_to(WIDTH // 2)
 
         arcade.draw_line(start_x, start_y, normalized.x, normalized.y, arcade.color.BLUE, 2)
 
