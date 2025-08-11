206610c40c934d627f1205248fe804c8496e3959
Dmytro <DmitryUglach@gmail.com>
2025-08-11 21:25:34 +0300

    refactoring

--- app/core/vectors_v2.py
+++ app/core/vectors_v2.py
@@ -1,3 +1,18 @@
 from collections import namedtuple
+from typing import Generic, TypeVar
 
-LightCustomVec2 = namedtuple("LightCustomVec2", ["x", "y"])
+from app.config import Y_MODIFIER
+
+LightVec2 = namedtuple("LightCustomVec2", ["x", "y"])
+
+T = TypeVar("T")
+
+class Vec2Transform(Generic[T]):
+
+    @staticmethod
+    def up(magnitude: T = 1):
+        return LightVec2(0, -magnitude * Y_MODIFIER)
+
+    @staticmethod
+    def down(magnitude: T = 1):
+        return LightVec2(0, magnitude * Y_MODIFIER)
