e2634573dbf0f885475732409e3b5c715a52a6d3
Dmytro <DmitryUglach@gmail.com>
2025-10-02 17:00:15 +0300

    event driven

--- app/core/geometry/shape.py
+++ app/core/geometry/shape.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from arcade import Texture
 
 from app.collections.animation_collection import AnimationCollection
-from app.config import NpcAnimations, CommonAnimations
+from app.config import CommonAnimations
 from app.core.geometry.types import Directions
 from app.protocols.collections.animation_collection_protocol import AnimationCollectionProtocol
 
@@ -12,7 +12,6 @@ class Shape:
     def __init__(self, icon_path: Path):
         self.icon_path: Path = icon_path
         self.animations: AnimationCollectionProtocol = AnimationCollection()
-        self.animation_mapping: dict[CommonAnimations, NpcAnimations] = {}
         self.current_animation: CommonAnimations = CommonAnimations.IDLE
         self.direction: Directions = Directions.FRONT
         pass
